9ecf21bd1dca5d44d7bb0e43d336297e66323938
vatsal <vatsalmunjpara3@gmail.com>
2025-05-09 15:13:14 +0530

    update env

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const port = process.env.PORT || 3000
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 //routes
 app.use('/auth/api',authRoutes)
